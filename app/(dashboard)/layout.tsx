fd2c64d7d48ddd06d80b5f461eeae65e68712b7f
ajkrizz <write4ajay2@gmail.com>
2024-02-05 11:23:00 +0530

    Initilaized Login page, Landing page and Prisma database

---
+++ app/(dashboard)/layout.tsx
@@ -0,0 +1,14 @@
+import DashboardNavbar from "@/components/ui/DashboardNavbar";
+
+export default function DashboardLayout({
+    children,
+    }: {
+        children?: React.ReactNode;
+        }) {
+            return (
+                <div className="flex flex-col h-full w-full">
+                    <DashboardNavbar/>
+                    {children}
+                </div>
+                );
+}
